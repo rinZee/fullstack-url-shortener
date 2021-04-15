298d49429ed328717c4dc8fb6597ac1860a70695
unknown <trex33160@gmail.com>
2021-04-15 20:07:17 +0545

    installed ts, axios, antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
-import Head from 'next/head'
+import Head from 'next/head' 
+import {useState} from 'react';
+import axios, { AxiosError } from 'axios'
+import { Alert, Button, Form, Input, Layout, Typography } from 'antd'
 import styles from '../styles/Home.module.css'
 
+const {Header, Content, Footer} = Layout;
+const {Title} = Typography;
+
+type ShortenLinkResponse = {
+short_link: string;
+}
+
+type ShortenLinkError = {
+  error: string;
+  error_description: string;
+}
+
+type FormValues = {
+  link: string;
+}
+
+
 export default function Home() {
+
+const [status, setStatus] = useState<'initial' | 'error' | 'success'>('initial');
+const [message, setMessage] = useState('');
+const [form] = Form.useForm(); 
+
+
   return (
     <div className={styles.container}>
       <Head>
